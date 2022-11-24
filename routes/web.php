22c3f63d3b10a6c98cedfe4ae199ad867d66ed85
Freeman Loco <ethanlobot@yahoo.com>
2022-11-24 23:40:10 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 // 静态页面
 // 两个参数：1、路由器相应请求路径 2、路由器将请求发送到x控制器中的x动作上
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 // 此命令将转移到 Controller 上处理
