9d08401ed87a3e1ec4da48b65d04c0db17f49d46
Freeman Loco <ethanlobot@yahoo.com>
2022-11-24 21:40:12 +0800

    模板切分

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- 第一个是变量、第二个是默认值 -->
+    <!-- 没有参数的时候，自动为默认值 -->
+    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
