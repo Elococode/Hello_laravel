552dbc962c0888c0ed6b8d98debf8d7f42c8c9c8
Freeman Loco <ethanlobot@yahoo.com>
2022-11-25 16:51:31 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// 静态页面
-// 两个参数：1、路由器相应请求路径 2、路由器将请求发送到x控制器中的x动作上
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
-// 此命令将转移到 Controller 上处理
+Route::resource('users', 'UsersController');
